1a72fc5a2dc3e1a64051a54a006e7bbcfb3dce13
Li Huang <putin.li@rock-chips.com>
2019-11-27 20:45:31 +0800

    Add validateBufferSize interface, for VTS case
        tip: run vts -m VtsHalGraphicsMapperV2_1Target
    
    Change-Id: Ib7055bea9e220762788e52e0260b932165ebf57b
    Signed-off-by: Li Huang <putin.li@rock-chips.com>

--- gralloc.cpp
+++ gralloc.cpp
@@ -508,6 +508,57 @@ static int drm_mod_open(const struct hw_module_t *mod,
 	return err;
 }
 
+#define BAD_VALUE 3
+
+static int drm_validate_buffer_size(const gralloc_module_t *mod, buffer_handle_t handle,
+            uint32_t w, uint32_t h, int32_t format, int usage, int layer_count,
+            uint32_t stride)
+{
+    int bpp;
+
+    struct gralloc_drm_handle_t* hnd = (struct gralloc_drm_handle_t*)handle;
+
+    if ( w > hnd->width )
+    {
+        ALOGE("validateBufferSize failed, width is invaild");
+        return BAD_VALUE;
+    }
+
+    if ( h > hnd->height )
+    {
+        ALOGE("validateBufferSize failed, height is invaild");
+        return BAD_VALUE;
+    }
+
+    bpp = gralloc_drm_get_bpp(hnd->format);
+    if (stride > (hnd->stride / bpp ) )
+    {
+        ALOGE("validateBufferSize failed, stride is invaild");
+        return BAD_VALUE;
+    }
+
+    if ( format != hnd->format )
+    {
+        if ( (format != HAL_PIXEL_FORMAT_YCbCr_420_888 || format != HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED) &&
+                hnd->format != HAL_PIXEL_FORMAT_YCrCb_NV12 )
+        {
+            ALOGE("validateBufferSize failed, format is invaild, format = %x, hndfmt = %x", format, hnd->format);
+            return BAD_VALUE;
+        }
+    }
+
+    if ( layer_count > 1 )
+    {
+        ALOGE("validateBufferSize failed, layer count is invaild");
+        return BAD_VALUE;
+    }
+
+    UNUSED(mod);
+    UNUSED(usage);
+
+    return 0;
+}
+
 static struct hw_module_methods_t drm_mod_methods = {
 	.open = drm_mod_open
 };
@@ -528,6 +579,7 @@ drm_module_t::drm_module_t()
     base.lock_ycbcr = drm_mod_lock_ycbcr;
     base.unlock = drm_mod_unlock;
     base.perform = drm_mod_perform;
+	base.validateBufferSize = drm_validate_buffer_size;
 
     mutex = PTHREAD_MUTEX_INITIALIZER;
     drm = NULL;
